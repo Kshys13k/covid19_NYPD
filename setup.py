7ccdc663c5c56c200012e46e8f32b133729545e5
kshys13k <kkterlecki@gmail.com>
2024-09-10 11:46:59 +0200

    changed project structure so it can be published as package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='covid19_NYPD',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "plotly",
+        "requests"
+    ],
+    author='Krzysiek Terlecki',
+    author_email='kkterlecki@gmail.com',
+    description='project for NYPD course, University of Warsaw Faculty of Mathematics, Informatics and Mechanics',
+    python_requires='>=3.6',
+)
